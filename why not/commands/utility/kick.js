2d13a440415897a964296cf1d2c0c5601f219c71
Mr chill <pirateking2405@gmail.com>
2021-05-17 23:00:58 +0530

    added kick.js

---
+++ why not/commands/utility/kick.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'kick',
+	description: 'kick',
+	async execute(message, args) {
+        if(!message.guild.me.hasPermission('KICK_MEMBERS')) return message.channel.send('I do not have permission')
+        const Member = message.mentions.members.first()
+        if(!Member) return message.channel.send('Please specify a member to kick');
+
+        await Member.kick({ reason: args.slice(1).join(" ") })
+        message.cnhannel.send(`${Member.user.tag} was kicked from the server!`)
+	},
+};
